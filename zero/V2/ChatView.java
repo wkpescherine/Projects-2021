735b24129002bdcd3231ddc32602ce199a8775f6
wkpescherine <wkpescherine@yahoo.com>
2021-01-08 23:38:45 -0500

    Add files via upload

--- zero/V2/ChatView.java
+++ zero/V2/ChatView.java
@@ -2,15 +2,25 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ChatView{
-    String chatMessage = "You are in the chat section";
+    String chatmessage = "You are in the chat section";
 
     JPanel chat = new JPanel();
-    JLabel chatmessage = new JLabel(chatMessage);
+    JPanel sideBar = new JPanel();
+    JPanel chatPanel = new JPanel();
+    JLabel chatMessage = new JLabel(chatmessage);
 
     ChatView(){
         chat.setLayout(new FlowLayout());
+        sideBar.setLayout(new FlowLayout());
+        chatPanel.setLayout(new FlowLayout());
         chat.setPreferredSize(new Dimension (840,520));
-        chat.add(chatmessage);
+        sideBar.setPreferredSize(new Dimension (300,520));
+        chatPanel.setPreferredSize(new Dimension (520,520));
+        sideBar.setBackground(Color.WHITE);
+        chatPanel.setBackground(Color.WHITE);
+        chatPanel.add(chatMessage);
+        chat.add(sideBar);
+        chat.add(chatPanel);
         chat.setVisible(false);
     }
-}//0
+}//26
