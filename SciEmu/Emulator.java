99eb6b244a11d5783bc9b26a0cfbcb49d06507a9
wkpescherine <wkpescherine@yahoo.com>
2021-01-25 22:12:49 -0500

    Add files via upload

--- SciEmu/Emulator.java
+++ SciEmu/Emulator.java
@@ -20,9 +20,17 @@ public class Emulator extends AppCompatActivity {
     String xString = "";
     String yString = "";
     String zString = "";
+    String aString = "";
+    String bString = "";
+    String cString = "";
+    String totalString = "";
     int x = 0;
     int y = 0;
     int z = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int total = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +62,8 @@ public class Emulator extends AppCompatActivity {
     public void additionText(View v){
         RelativeLayout main = findViewById(R.id.main);
         main.setVisibility(View.VISIBLE);
+        RelativeLayout unique = findViewById(R.id.unique);
+        unique.setVisibility(View.GONE);
         x = 0;
         y = 0;
         z = 0;
@@ -77,6 +87,8 @@ public class Emulator extends AppCompatActivity {
     public void subtractionText(View v){
         RelativeLayout main = findViewById(R.id.main);
         main.setVisibility(View.VISIBLE);
+        RelativeLayout unique = findViewById(R.id.unique);
+        unique.setVisibility(View.GONE);
         x = 0;
         y = 0;
         z = 0;
@@ -100,6 +112,8 @@ public class Emulator extends AppCompatActivity {
     public void divideText(View v){
         RelativeLayout main = findViewById(R.id.main);
         main.setVisibility(View.VISIBLE);
+        RelativeLayout unique = findViewById(R.id.unique);
+        unique.setVisibility(View.GONE);
         x = 0;
         y = 0;
         z = 0;
@@ -123,6 +137,8 @@ public class Emulator extends AppCompatActivity {
     public void multiplyText(View v){
         RelativeLayout main = findViewById(R.id.main);
         main.setVisibility(View.VISIBLE);
+        RelativeLayout unique = findViewById(R.id.unique);
+        unique.setVisibility(View.GONE);
         x = 0;
         y = 0;
         z = 0;
@@ -160,34 +176,42 @@ public class Emulator extends AppCompatActivity {
         TextView textZ = findViewById(R.id.Zvalue);
         TextView textMessageSub = findViewById(R.id.message);
         textMessageSub.setText("2D Area selected");
-        textX.setText("x");
+        textX.setText("h");
         textS.setText("*");
-        textY.setText("y");
+        textY.setText("w");
         textE.setText("=");
-        textZ.setText("z");
+        textZ.setText("area");
     }
     public void Area3D(View v){
         RelativeLayout main = findViewById(R.id.main);
-        main.setVisibility(View.VISIBLE);
-        x = 0;
-        y = 0;
-        z = 0;
-        xString = "";
-        yString = "";
-        zString = "";
-        subCategory = "Area2D";
-        TextView textX = findViewById(R.id.Xvalue);
-        TextView textS = findViewById(R.id.sign);
-        TextView textY = findViewById(R.id.Yvalue);
-        TextView textE = findViewById(R.id.equals);
-        TextView textZ = findViewById(R.id.Zvalue);
+        main.setVisibility(View.GONE);
+        RelativeLayout unique = findViewById(R.id.unique);
+        unique.setVisibility(View.VISIBLE);
+        a = 0;
+        b = 0;
+        c = 0;
+        total = 0;
+        aString = "";
+        bString = "";
+        cString = "";
+        totalString = "";
+        subCategory = "Area3D";
+        TextView textA = findViewById(R.id.Avalue);
+        TextView textS1 = findViewById(R.id.sign1);
+        TextView textB = findViewById(R.id.Bvalue);
+        TextView textS2 = findViewById(R.id.sign2);
+        TextView textC = findViewById(R.id.sign2);
+        TextView textE1 = findViewById(R.id.equals1);
+        TextView texttot = findViewById(R.id.total);
         TextView textMessageSub = findViewById(R.id.message);
-        textMessageSub.setText("2D Area selected");
-        textX.setText("x");
-        textS.setText("*");
-        textY.setText("y");
-        textE.setText("=");
-        textZ.setText("z");
+        textMessageSub.setText("3D Area selected");
+        textA.setText("h");
+        textS1.setText("*");
+        textB.setText("w");
+        textS2.setText("*");
+        textC.setText("d");
+        textE1.setText("=");
+        texttot.setText("total");
     }
 
     public void focusX(View v){
@@ -202,6 +226,21 @@ public class Emulator extends AppCompatActivity {
     }
     public void focusZ(View v){
         focus = "Z";
+    }
+    public void focusA(View v){
+        focus = "A";
+        TextView textMessageA = findViewById(R.id.Avalue);
+        textMessageA.setText("focus on a selected");
+    }
+    public void focusB(View v){
+        focus = "B";
+        TextView textMessageA = findViewById(R.id.Bvalue);
+        textMessageA.setText("focus on b selected");
+    }
+    public void focusC(View v){
+        focus = "C";
+        TextView textMessageA = findViewById(R.id.Cvalue);
+        textMessageA.setText("focus on c selected");
     }
 
     public void setValues(int val){
@@ -233,6 +272,47 @@ public class Emulator extends AppCompatActivity {
             valueY.setText(yString);
         }
 
+        if(focus.equals("A") && a != 0){
+            TextView valueA = findViewById(R.id.Avalue);
+            aString  += val;
+            a = Integer.parseInt(aString);
+            valueA.setText(aString);
+        }
+
+        if(focus.equals("A") && a == 0 ){
+            TextView valueA = findViewById(R.id.Avalue);
+            aString  = val+"";
+            a = Integer.parseInt(aString);
+            valueA.setText(aString);
+        }
+
+        if(focus.equals("B") && b != 0){
+            TextView valueB = findViewById(R.id.Bvalue);
+            bString  += val;
+            b = Integer.parseInt(bString);
+            valueB.setText(bString);
+        }
+
+        if(focus.equals("B") && b == 0 ){
+            TextView valueB = findViewById(R.id.Bvalue);
+            bString  = val+"";
+            b = Integer.parseInt(bString);
+            valueB.setText(bString);
+        }
+
+        if(focus.equals("C") && c != 0){
+            TextView valueC = findViewById(R.id.Cvalue);
+            cString  += val;
+            c = Integer.parseInt(cString);
+            valueC.setText(cString);
+        }
+
+        if(focus.equals("C") && c == 0 ){
+            TextView valueC = findViewById(R.id.Cvalue);
+            cString  = val+"";
+            c = Integer.parseInt(cString);
+            valueC.setText(cString);
+        }
     }
 
     public void clickToSolve(View v){
@@ -266,12 +346,18 @@ public class Emulator extends AppCompatActivity {
             TextView valueZ = findViewById(R.id.Zvalue);
             valueZ.setText(zString);
         }
+        if(subCategory.equals("Area2D")){
+            total=a*b*c;
+            totalString = total+"";
+            TextView valueTotal = findViewById(R.id.total);
+            valueTotal.setText(totalString);
+        }
         TextView textMessageSolve = findViewById(R.id.message);
         textMessageSolve.setText("Solved");
         //This works for calling the formulas from the formula class/instance
         //z = basicFormula.addition(2,4);
-        //textMessageSolve.setText(z+"");
         //Above will be used eventually to replace current  forumlas.
+        textMessageSolve.setText(z+"");
     }
 
     public void displaySubs(int num){
