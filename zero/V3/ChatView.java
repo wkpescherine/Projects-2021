72f7b1a41326cb9677c19c63cdc5dc8673c0ead0
wkpescherine <wkpescherine@yahoo.com>
2021-02-08 23:53:18 -0500

    Add files via upload

--- zero/V3/ChatView.java
+++ zero/V3/ChatView.java
@@ -5,17 +5,20 @@ import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ChatView implements ActionListener{
     String personToMessage = "";
+    String chatUserName = "";
+    String [] friendsList = new String[100];
+    String [] invites = new String[100];
 
     JPanel chat = new JPanel();
     JPanel sideBar = new JPanel();
     JPanel chatPanel = new JPanel();
-    JPanel personInvites = new JPanel();
+    JPanel friendsPanel = new JPanel();
     JPanel messagePanel = new JPanel();
     JLabel messagePerson = new JLabel("Send To :"+personToMessage);
     JTextField messageArea = new JTextField();
@@ -24,6 +27,7 @@ public class ChatView implements ActionListener{
     JButton sendMessage = new JButton("Message");
 
     ChatView(){
+        buildFriendsSection();
         findPersonBtn.addActionListener(this);
 
         chat.setLayout(new FlowLayout());
@@ -55,7 +59,7 @@ public class ChatView implements ActionListener{
             FileWriter myWriter = new FileWriter("friendsListDB.txt", true);
             BufferedWriter bWriter = new BufferedWriter(myWriter);
             PrintWriter pWriter = new PrintWriter(bWriter);
-            pWriter.write(invitePerson+"\r");
+            pWriter.write(chatUserName+"," +invitePerson+"," +"INVITE"+"\r");
             pWriter.close();
         }catch(IOException ioe){
             ioe.printStackTrace();
@@ -63,4 +67,48 @@ public class ChatView implements ActionListener{
         findPersonArea.setText("");
         System.out.println("Invited a person");
     }
-}//66
+
+    public void buildFriendsSection(){
+        try{
+            int invitesNum = 0;
+            File myFile = new File("friendsListDB.txt");
+			Scanner myReader = new Scanner(myFile);
+            while(myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                invites[invitesNum] = (data);
+				String [] invite = data.split(","); 
+				if(invite[1].equals(chatUserName)){
+                    invitesNum += 1;
+				}
+			}
+            myReader.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        
+        for(int i = 0; i < invites.length-1; i++){
+            friendsPanel.removeAll();
+            String subInvite = invites[i];
+            String [] subInviteSplit = subInvite.split(",");
+            if(subInviteSplit[1].equals(chatUserName)){
+                friendsPanel.removeAll();
+                int start = invites.length;
+                JPanel jpanel[] = new JPanel[start];
+                JLabel jlabel[] = new JLabel[start];
+                for(int x = 0; x <invites.length; ){
+                    jpanel[x] = new JPanel();
+                    jlabel[x] = new JLabel();
+                    JPanel test = jpanel[x];
+                    JLabel testLabel = jlabel[x];
+                    test.setPreferredSize(new Dimension (100,20));
+                    test.setBackground(Color.RED);
+                    test.add(testLabel);
+                    System.out.println("Clicked on the blank2");
+                    friendsPanel.add(test);
+                }
+                friendsPanel.validate();
+                friendsPanel.repaint();
+            }
+        }
+    }
+}//114
