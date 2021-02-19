afa1654add84f8dbe54fceec5374c3556224ffbe
wkpescherine <wkpescherine@yahoo.com>
2021-02-19 00:41:42 -0500

    Add files via upload

--- zero/V3/Settings.java
+++ zero/V3/Settings.java
@@ -1,24 +1,33 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Settings{
+public class Settings implements ActionListener{
     JPanel setting = new JPanel();
-    JLabel settingText = new JLabel("In setting section");
-    JLabel tempId = new JLabel("Temp Id");
+    JLabel settingText = new JLabel("");
+    JLabel tempId = new JLabel("");
+    JButton configure = new JButton("Configure");
     ConfigFile settingConfigFile = new ConfigFile();
 
     String settingUserName = "none";
     String settingsTempId = "none";
 
     Settings(){
+        configure.addActionListener(this);
+
         setting.setLayout(new FlowLayout());
         setting.setPreferredSize(new Dimension (840,520));
-        settingText.setPreferredSize(new Dimension (640,50));
-        settingText.setText(settingUserName);
-        tempId.setPreferredSize(new Dimension (640,50));
-        tempId.setText(settingsTempId);
+        settingText.setPreferredSize(new Dimension (640,30));
+        tempId.setPreferredSize(new Dimension (640,30));
+        configure.setPreferredSize(new Dimension (640,50));
         setting.add(settingText);
         setting.add(tempId);
+        setting.add(configure);
         setting.setVisible(false);
     }
-}//14
+
+    public void actionPerformed(ActionEvent e){
+        settingText.setText(settingConfigFile.username);
+        tempId.setText(settingsTempId);
+    }
+}//33
