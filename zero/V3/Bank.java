2ebffdf864998613a263f0d116d51a674ec788a2
wkpescherine <wkpescherine@yahoo.com>
2021-02-21 23:28:41 -0500

    Add files via upload

--- zero/V3/Bank.java
+++ zero/V3/Bank.java
@@ -27,7 +27,9 @@ public class Bank implements ActionListener{
         bank.setLayout(new FlowLayout());
         bank.setPreferredSize(new Dimension (840,520));
         purchaseZ.setPreferredSize(new Dimension (400,30));
+        countryToZ.setBackground(Color.WHITE);
         sellZ.setPreferredSize(new Dimension (400,30));
+        zToCountry.setBackground(Color.WHITE);
         bankText.setPreferredSize(new Dimension (600,30));
         countryToZ.setPreferredSize(new Dimension(600,300));
         zToCountry.setPreferredSize(new Dimension(600,300));
@@ -44,9 +46,43 @@ public class Bank implements ActionListener{
 	public void actionPerformed(ActionEvent e){
         countryToZ.setVisible(true);
         zToCountry.setVisible(false);
+        conversionPanelZ();
+    }
+
+    public void conversionPanelZ(){
+        countryToZ.removeAll();
+        int start = country.length;
+        JPanel jpanel[] = new JPanel[start];
+        JButton jbutton[] = new JButton[start];
+        JTextField jtextfield[] = new JTextField[start];
+        JLabel jlabelrate[] = new JLabel[start];
+        JLabel jlabelcountry[] = new JLabel[start];
     }
 
     public void convertToZcoin(){}
 
     public void cenvertToCountry(){}
-}//52
+}//65
+
+for(int a = 0; a < emails.length-1; a++){
+jpanel[a] = new JPanel();
+jbutton[a] = new JButton("View");
+jlabel[a] = new JLabel();
+JPanel mailSub = jpanel[a];
+JButton mailSubButton = jbutton[a];
+JLabel mailSubLabel = jlabel[a];
+mailSubButton.addActionListener(
+    new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            displayMessage(subMsgSplit[3]);
+            zmail.setVisible(false);
+            messagePanel.setVisible(true);
+        }
+    }
+);
+mailSubLabel.setText(subMsgSplit[1]+" "+ subMsgSplit[2]);
+mailSub.setLayout(new FlowLayout());
+mailSub.setPreferredSize(new Dimension (600,50));
+mailSub.add(mailSubLabel);
+mailSub.add(mailSubButton);
+zmail.add(mailSub);
