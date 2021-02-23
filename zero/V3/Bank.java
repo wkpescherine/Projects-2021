98bf804e7472b8873bd274f2e87c8fac9d0135c0
wkpescherine <wkpescherine@yahoo.com>
2021-02-22 23:18:38 -0500

    Add files via upload

--- zero/V3/Bank.java
+++ zero/V3/Bank.java
@@ -8,10 +8,11 @@ public class Bank implements ActionListener{
     JPanel zToCountry = new JPanel();
     JButton purchaseZ = new JButton("Purchase Zcoins");
     JButton sellZ = new JButton("Sell Zcoins");
+    JButton completeAction = new JButton("NONE");
     JLabel bankText = new JLabel("Zero Financials");
 
-    String [] country = {};
-    Float [] conversionRate = {};
+    String [] country = {"Mexico", "Canada", "EU", "Turkish Lira", "Chinese Yen"};
+    Double [] conversionRate = { 4.12 , 1.12, 1.50, 5.85 , 500.0 };
 
     Bank(){
         purchaseZ.addActionListener(this);
@@ -20,26 +21,30 @@ public class Bank implements ActionListener{
 				public void actionPerformed(ActionEvent e){
                     countryToZ.setVisible(false);
                     zToCountry.setVisible(true);
+                    convertToCurrency();
 				}
 			}
         );
 
         bank.setLayout(new FlowLayout());
         bank.setPreferredSize(new Dimension (840,520));
-        purchaseZ.setPreferredSize(new Dimension (400,30));
-        countryToZ.setBackground(Color.WHITE);
-        sellZ.setPreferredSize(new Dimension (400,30));
-        zToCountry.setBackground(Color.WHITE);
+        purchaseZ.setPreferredSize(new Dimension (350,30));
+        sellZ.setPreferredSize(new Dimension (350,30));
         bankText.setPreferredSize(new Dimension (600,30));
-        countryToZ.setPreferredSize(new Dimension(600,300));
-        zToCountry.setPreferredSize(new Dimension(600,300));
+        bankText.setHorizontalAlignment(SwingConstants.CENTER);
+        countryToZ.setPreferredSize(new Dimension(650,300));
+        countryToZ.setBackground(Color.WHITE);
+        zToCountry.setPreferredSize(new Dimension(650,300));
+        zToCountry.setBackground(Color.WHITE);
         countryToZ.setVisible(false);
         zToCountry.setVisible(false);
+        completeAction.setVisible(false);
         bank.add(bankText);
         bank.add(purchaseZ);
         bank.add(sellZ);
         bank.add(countryToZ);
         bank.add(zToCountry);
+        bank.add(completeAction);
         bank.setVisible(false);
     }
 
@@ -62,8 +67,8 @@ public class Bank implements ActionListener{
             jpanel[a] = new JPanel();
             jbutton[a] = new JButton("Purchase");
             jlabelcountry[a] = new JLabel(country[a]);
-            jlabelrate[a] = new JLabel(country[a]+"");
-            jtextfield[a] = new JTextField(20);
+            jlabelrate[a] = new JLabel(conversionRate[a]+"");
+            jtextfield[a] = new JTextField(10);
             JPanel countryList = jpanel[a];
             JButton purchase = jbutton[a];
             JLabel countryName = jlabelcountry[a];
@@ -72,12 +77,18 @@ public class Bank implements ActionListener{
             purchase.addActionListener(
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e){
-                        System.out.println("Test");
+                        Double zcoins = Double.parseDouble(countryValue.getText());
+                        Double zCoinRate = Double.parseDouble(countryRate.getText());
+                        convertToZcoin(zcoins, zCoinRate);
                     }
                 }
             );
-            countryList.setPreferredSize(new Dimension(400,30));
             countryList.setLayout(new FlowLayout());
+            countryList.setPreferredSize(new Dimension(600,50));
+            countryName.setPreferredSize(new Dimension(120,30));
+            countryValue.setPreferredSize(new Dimension(120,30));
+            countryRate.setPreferredSize(new Dimension(120,30));
+            purchase.setPreferredSize(new Dimension(120,30));
             countryList.add(countryName);
             countryList.add(countryValue);
             countryList.add(countryRate);
@@ -89,7 +100,77 @@ public class Bank implements ActionListener{
         countryToZ.repaint();
     }
 
-    public void convertToZcoin(){}
+    public void convertToCurrency(){
+        zToCountry.removeAll();
+        int start = country.length;
+        JPanel jpanel[] = new JPanel[start];
+        JButton jbutton[] = new JButton[start];
+        JTextField jtextfield[] = new JTextField[start];
+        JLabel jlabelrate[] = new JLabel[start];
+        JLabel jlabelcountry[] = new JLabel[start];
 
-    public void cenvertToCountry(){}
-}//92
+        for(int a = 0; a < country.length; a++){
+            jpanel[a] = new JPanel();
+            jbutton[a] = new JButton("Sell");
+            jlabelcountry[a] = new JLabel(country[a]);
+            jlabelrate[a] = new JLabel(conversionRate[a]+"");
+            jtextfield[a] = new JTextField(10);
+            JPanel countryList = jpanel[a];
+            JButton sell = jbutton[a];
+            JLabel countryName = jlabelcountry[a];
+            JLabel countryRate = jlabelrate[a];
+            JTextField  countryValue = jtextfield[a];
+            sell.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        Double zcoins = Double.parseDouble(countryValue.getText());
+                        Double zCoinRate = Double.parseDouble(countryRate.getText());
+                        convertToCountry(zcoins,zCoinRate);
+                    }
+                }
+            );
+            countryList.setLayout(new FlowLayout());
+            countryList.setPreferredSize(new Dimension(600,50));
+            countryName.setPreferredSize(new Dimension(120,50));
+            countryValue.setPreferredSize(new Dimension(120,50));
+            countryRate.setPreferredSize(new Dimension(120,50));
+            sell.setPreferredSize(new Dimension(120,50));
+            countryList.add(countryValue);
+            countryList.add(countryRate);
+            countryList.add(countryName);
+            countryList.add(sell);
+            zToCountry.add(countryList);
+        }
+
+        zToCountry.validate();
+        zToCountry.repaint();
+    }
+
+    public void convertToZcoin(Double zc, Double zr){
+        Double price = zc*zr;
+        completeAction.setVisible(true);
+        completeAction.setText("Purchase "+zc+" at a Rate of "+zr+" for a cost of "+price+" click here to complete");
+        completeAction.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    completeAction.setVisible(false);
+                }
+            }
+        );
+        conversionPanelZ();
+    }
+
+    public void convertToCountry(Double zc, Double zr){
+        Double price = zr*zc;
+        completeAction.setVisible(true);
+        completeAction.setText("Selling "+zc+" at a Rate of "+zr+" for a price of "+price+" click here to complete");
+        completeAction.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    completeAction.setVisible(false);
+                }
+            }
+        );
+        conversionPanelZ();
+    }
+}//176
--- zero/V3/Site.java
+++ zero/V3/Site.java
@@ -1,20 +1,31 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.lang.String;
 
-public class Site{
+public class Site implements ActionListener{
     JPanel site = new JPanel();
 	JLabel siteText = new JLabel("This is a test site");
 	String testsite2 = "Zero;p(name,100,100, none);t(Welcome to a Zero Site)";
 	String testsite3 = "Zero;p(name,100,100, none);name(t(This is a string text))";
 	String [] groupNames = {"group_name0","group_name1","group_name2","group_name3"};
+	JPanel adSpace = new JPanel();
 
     Site(){
+		adSpace.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Clicked in Ad Space");
+			}
+		}
+		);
+
 		site.removeAll();
         site.setLayout(new FlowLayout());
         site.setPreferredSize(new Dimension (860,540));
 		site.setVisible(false);
 		site.add(siteText);
+		site.add(adSpace);
 		site.validate();
 		site.repaint();
 	}
@@ -51,6 +62,19 @@ public class Site{
 				testLabel.setText(subSections[1]);
 				site.add(testLabel);
 			}
+			//b(group_name, button_label)
+			if(subSections[0].equals("b")){
+				jbutton[i] = new JButton();
+				JButton testBtn = jbutton[i];
+				testBtn.addActionListener(
+					new ActionListener(){
+						public void actionPerformed(ActionEvent e){
+							System.out.println("Button Stub");
+						}
+					}
+				);
+				site.add(testBtn);
+			}
 			for(int x = 0; x <4; x ++){
 				if(groupNames[x].equals(subSections[0])){
 					//name(t(This is a test));
@@ -71,6 +95,20 @@ public class Site{
 						test2.setBackground(Color.RED);
 						jpanel[x].add(test2);
 					}
+					//name(b(none, test Button))
+					if(subSections[1].equals("b")){
+						groupNames[i] = subSections[2];
+						jbutton[i] = new JButton();
+						JButton testBtn = jbutton[i];
+						testBtn.addActionListener(
+							new ActionListener(){
+								public void actionPerformed(ActionEvent e){
+									System.out.println("Button Stub");
+								}
+							}
+						);
+						jpanel[x].add(testBtn);
+					}
 				}
 			}
 			
@@ -78,4 +116,8 @@ public class Site{
 		site.validate();
 		site.repaint();
 	}
-}//81
+
+	public void actionPerformed(ActionEvent e){
+        System.out.println("You clicked where an ad can go eventually");
+    }
+}//123
