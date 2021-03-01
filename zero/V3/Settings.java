5685805049185d74eae96e3d9d1684f454412aca
wkpescherine <wkpescherine@yahoo.com>
2021-02-28 23:50:28 -0500

    Add files via upload

--- zero/V3/Settings.java
+++ zero/V3/Settings.java
@@ -8,18 +8,24 @@ public class Settings implements ActionListener{
     JLabel tempId = new JLabel("");
     JLabel settingDate = new JLabel("");
     JLabel settingVersion = new JLabel("");
-    JButton configure = new JButton("Configure");
-    //ConfigFile settingConfigFile = new ConfigFile();
-    //Config settingConfig = new Config();
+    JRadioButton w1050_700 = new JRadioButton("1050x700");
+    JRadioButton w900_600 = new JRadioButton("900x600");
+    JRadioButton w750_500 = new JRadioButton("750x500");
+    JRadioButton w600_400 = new JRadioButton("600x400"); 
+    JRadioButtonMenuItem windowSize = new JRadioButtonMenuItem();
 
     private Config configSetting ;
 
-    //String settingUserName = "none";
-    //String settingsTempId = "none";
-
     Settings(){
-        //configSetting = setConfig;
-        configure.addActionListener(this);
+        w1050_700.addActionListener(this);
+        w900_600.addActionListener(this);
+        w750_500.addActionListener(this);
+        w600_400.addActionListener(this);
+
+        w1050_700.setActionCommand("1050x700");
+        w900_600.setActionCommand("900x600");
+        w750_500.setActionCommand("750x500");
+        w600_400.setActionCommand("600x400");
 
         setting.setLayout(new FlowLayout());
         setting.setPreferredSize(new Dimension (840,520));
@@ -27,26 +33,53 @@ public class Settings implements ActionListener{
         settingDate.setPreferredSize(new Dimension (640,30));
         settingVersion.setPreferredSize(new Dimension (640,30));
         tempId.setPreferredSize(new Dimension (640,30));
-        configure.setPreferredSize(new Dimension (640,50));
+        windowSize.setLayout(new FlowLayout());
+        windowSize.setPreferredSize(new Dimension(640,60));
+        w1050_700.setPreferredSize(new Dimension(300,25));
+        w900_600.setPreferredSize(new Dimension(300,25));
+        w750_500.setPreferredSize(new Dimension(300,25));
+        w600_400.setPreferredSize(new Dimension(300,25));
+        windowSize.add(w1050_700);
+        windowSize.add(w900_600);
+        windowSize.add(w750_500);
+        windowSize.add(w600_400);
         setting.add(settingText);
         setting.add(settingDate);
         setting.add(settingVersion);
         setting.add(tempId);
-        setting.add(configure);
+        setting.add(windowSize);
         setting.setVisible(false);
     }
 
     public void settingConfigs(Config config){
         configSetting = config;
+        settingDate.setText(configSetting.date);
+        settingText.setText(configSetting.username);
+        tempId.setText(configSetting.tempId);
+        settingVersion.setText(configSetting.version+"");
     }
 
     public void actionPerformed(ActionEvent e){
-        System.out.println(configSetting.username);
-        //System.out.println(config.tempId);
-        //settingDate.setText(settingConfigFile.date);
-        settingText.setText(configSetting.username);
-        //settingText.setText(config.username);
-        tempId.setText(configSetting.tempId);
-        //settingVersion.setText(settingConfigFile.version);
+        String value = e.getActionCommand();
+        if(value.equals("1050x700")){
+            w900_600.setSelected(false);
+            w750_500.setSelected(false);
+            w600_400.setSelected(false);
+        }
+        if(value.equals("900x600")){
+            w1050_700.setSelected(false);
+            w750_500.setSelected(false);
+            w600_400.setSelected(false);
+        }
+        if(value.equals("750x500")){
+            w1050_700.setSelected(false);
+            w900_600.setSelected(false);
+            w600_400.setSelected(false);
+        }
+        if(value.equals("600x400")){
+            w1050_700.setSelected(false);
+            w900_600.setSelected(false);
+            w750_500.setSelected(false);
+        }
     }
-}//52
+}//85
