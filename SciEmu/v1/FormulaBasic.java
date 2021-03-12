0f165563ea01741899e1f2961a5f4bc8033b215b
wkpescherine <wkpescherine@yahoo.com>
2021-03-11 23:49:24 -0500

    Add files via upload

---
+++ SciEmu/v1/FormulaBasic.java
@@ -0,0 +1,10 @@
+package com.example.sciemu;
+
+import android.widget.TextView;
+
+public class FormulaBasic {
+    static int addition(int x, int y){
+        int Ztotal = x+y;
+        return Ztotal;
+    }
+}//10
