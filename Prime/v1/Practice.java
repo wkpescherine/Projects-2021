1c02e09263142f44eae329d572dce07aae678c82
wkpescherine <wkpescherine@yahoo.com>
2021-05-25 22:26:04 -0400

    Add files via upload

---
+++ Prime/v1/Practice.java
@@ -0,0 +1,18 @@
+package com.example.primeacademy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Practice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+}//18
