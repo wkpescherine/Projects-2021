0fb4a383c114c1c40d6bc70be97f0320ddb8c7eb
wkpescherine <wkpescherine@yahoo.com>
2021-06-03 23:36:15 -0400

    Add files via upload

--- Prime/Web_V1/App.js
+++ Prime/Web_V1/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Main from './main'
 import Home from './home'
 import Create from "./create"
+import Lesson from "./lesson"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component = {Main} />
         <Route exact path="/home" component = {Home} />
         <Route exact path="/create" component = {Create} />
+        <Route exact path="/lesson" component = {Lesson}/>
       </Router>
     </div>
   );
---
+++ Prime/Web_V1/lesson.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+import {Redirect} from 'react-router-dom'
+
+class Lesson extends React.Component {
+    state={
+        section: "none" 
+    }
+
+    homeButton( something){
+        this.setState({
+            section: something
+        })
+    }
+    render(){
+        if(this.state.section === "home"){
+            if(this.state.section === "home"){
+                return <Redirect to="/home"/>
+            }
+        }
+        if(this.state.section === "profile"){
+            return <Redirect to="/"/>
+        }
+        if(this.state.section !=="none" && this.state.section !== "lesson"){
+            return <Redirect to="/lesson"/>
+        }
+        return (
+        <div>
+            <h3> Prime Academy</h3>
+                <div>
+                    <button onClick={()=>this.homeButton("home")}> Home</button>
+                    <button onClick={()=>this.homeButton("profile")}> Profile</button>
+                    <button onClick={()=>this.homeButton("lesson")}> Lessons</button>
+                </div>
+            <h1>In Lesson Section</h1>
+        </div>
+      );
+    }
+}
+
+export default Lesson;
