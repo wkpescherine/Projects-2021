55aaed39fb75f60533e16160e3f804c8fbd69d4e
wkpescherine <wkpescherine@yahoo.com>
2021-06-06 00:21:36 -0400

    Add files via upload

---
+++ Prime/Web_V1/c++.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+class CPlusPlus extends React.Component {
+    render(){
+        <div>
+                <h5>This is an HTML section text</h5>
+        </div>
+    }
+}
+export default CPlusPlus;
