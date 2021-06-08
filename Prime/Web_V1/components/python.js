529b606d5702dfcfd970e54f59c0de2bf2a9d953
wkpescherine <wkpescherine@yahoo.com>
2021-06-07 22:21:00 -0400

    Add files via upload

---
+++ Prime/Web_V1/components/python.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+class Python extends React.Component {
+    render(){
+        return (
+            <div>
+                <h5>This is a Python section text</h5>
+            </div>
+        )
+    }
+}
+export default Python;
