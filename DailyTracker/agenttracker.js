52ec93b18e6dd61ff77d300f504cd208513421e4
wkpescherine <wkpescherine@yahoo.com>
2021-07-05 01:17:59 -0400

    Add files via upload

--- DailyTracker/agenttracker.js
+++ DailyTracker/agenttracker.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import './App.css';
 
+import Agent from "./agent"
+
 class AgentTracker extends React.Component {
     render(){
         return (
-            <div style={{
-                display: "flex",
-                justifyContent: "center"
-                }}>
+            <div>
                 <div>
-
+                    < Agent data1={"Agent"} data2={"Today"} data3={"Weekly"} data4={"Monthly"}/>
                 </div>
-                <div style={{width: 100, border: "2px solid white"}}>
-                    <div style={{display:"inline", color: "white"}}>
-                        Agent Name
-                    </div>
+                <div>
+                    < Agent data1={"pescherine"} data2={"0"} data3={"0"} data4={"0"}/>
                 </div>
-                <div style={{width: 100, border: "2px solid white"}}>
-                    <div style={{display:"inline", color: "white", width: 100}}>
-                        0
-                    </div>
+                <div>
+                    < Agent data1={"aryehb"} data2={"0"} data3={"0"} data4={"0"}/>
                 </div>
-                <div style={{width: 100, border: "2px solid white"}}>
-                    <div style={{display:"inline", color: "white", width: 100}}>
-                        0
-                    </div>
+                <div>
+                    < Agent data1={"lashanski"} data2={"0"} data3={"0"} data4={"0"}/>
                 </div>
-                <div style={{width: 100, border: "2px solid white"}}>
-                    <div style={{display:"inline", color: "white", width: 100}}>
-                        0
-                    </div>
+                <div>
+                    < Agent data1={"lakshmi"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"michael"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"kevin"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"princess"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"teejay"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"james"} data2={"0"} data3={"0"} data4={"0"}/>                
+                </div>
+                <div>
+                    < Agent data1={"angelia"} data2={"0"} data3={"0"} data4={"0"}/>
+                </div>
+                <div>
+                    < Agent data1={"someone"} data2={"0"} data3={"0"} data4={"0"}/>
+                </div>
+                <div>
+                    < Agent data1={"forgotten"} data2={"0"} data3={"0"} data4={"0"}/>
                 </div>
             </div>
         )
