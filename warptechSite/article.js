5ff3edbfb009e0c01b8cf9515834f31c81a627af
wkpescherine <wkpescherine@yahoo.com>
2021-07-19 23:46:41 -0400

    Add files via upload

--- warptechSite/article.js
+++ warptechSite/article.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import HomeArticle from "./home_article"
+
 class Article extends React.Component {
     render(){
         return (
@@ -10,7 +12,16 @@ class Article extends React.Component {
                 }}>
                 <div style={{width: 400}}>
                     <div style={{display:"inline", color: "white"}}>
-                        <p>Welcome to WARP Technology the home of a new and reality bending company that will take all of the norms that we have thought about. And turn them on end.</p>
+                        { this.props.data1 === "home" &&
+                            <HomeArticle />
+                        }
+                    </div>
+                </div>
+                <div style={{width: 400}}>
+                    <div style={{display:"inline", color: "white"}}>
+                        { this.props.data1 === "sportforge" &&
+                            <p style={{display:"inline", color: "white"}}>A sports based web app that allows you to play Iron man challenges on a daily basis using live games</p>
+                        }
                     </div>
                 </div>
             </div>
